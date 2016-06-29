47a4942a785e5a6abe46e327db950dd2879b966a
Omer <ramic.omer82@gmail.com>
2016-06-29 13:50:03 +0200

    Update login-page-finder.py
    Added parser instead input

--- login-page-finder.py
+++ login-page-finder.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-# This was written for educational purpose only. Use it at your own risk.
-# Author will not be responsible for any damage!
+# Use it at your own risk. Author will not be responsible for any damage!
 
+import argparse
 from sys import stdout
 from signal import signal, getsignal, SIGINT
 from urllib.request import Request, urlopen, URLError, HTTPError
 
+DESCRIPTION, AUTHOR, VERSION, UA, TIMEOUT = 'Login page finder script', 'Author: Omer Ramić <@sp_omer>', '0.2f', {'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64) Gecko/20071127 Firefox/2.0.0.11'}, 30
+
 class FindLoginPage():
-	"""This class checks for login page"""
+	"""Main object for the script"""
 	def __init__ 	(
 					self:'pointing to self',
 					domain:'domain to search login page for',
-					headers:'browser headers, some web sites require headers to open',
 					sub_links:'sub links list for domain to search login page for',
 					number_of_sub_links:'number of lines file linkovi.txt contains'
 					):
 						self.domain = domain
-						self.headers = headers
 						self.sub_links = sub_links
 						self.number_of_sub_links = number_of_sub_links
 						self.sub_link_counter = 0
@@ -30,68 +30,52 @@ class FindLoginPage():
 				break
 			try:
 				full_link = self.domain + '/' + sub_link.strip()
-				request_link = Request(full_link, headers=self.headers)
+				request_link = Request(full_link, headers=UA)
 				self.sub_link_counter+=1
-				stdout.write("\r" + "Broj provjerenih linkova: %s/%s" %(self.sub_link_counter, self.number_of_sub_links))
+				stdout.write("\r" + "Nr. of checked links: %s/%s" %(self.sub_link_counter, self.number_of_sub_links))
 				stdout.flush()
-				response = urlopen(request_link)
+				response = urlopen(request_link, timeout=TIMEOUT)
 			except HTTPError:
 				pass
 			except URLError:
 				pass
 			else:
 				self.valid_number_of_links+=1
-				print ("\nPronađen link: " + full_link)
-		stdout.write("\n\n"+"Ukupan broj pronađenih validnih stranica: %s\n" %self.valid_number_of_links)
+				print ("\nValid link found: " + full_link)
+		stdout.write("\n\n"+"Number of valid links found: %s\n" %self.valid_number_of_links)
 
-def load_data():
-	"""This function sets all needed variables, checks if domain exists and loads file contests if file exist"""
+def load_data(full_domain):
+	"""Load file contests if file exist, then check and test if domain exists"""
+	print ("\n" + DESCRIPTION + " #v" + VERSION + "\n  " + AUTHOR + "\n")
 	try:
 		with open('linkovi.txt', 'r') as file:
 			sub_links = (file.readlines())
 			sub_link_count=len(sub_links)
-
-		domain = input('Unesite link stranice u sledećem obliku \n(npr: stranica.com or www.stranica.com )>> ')
-		headers = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64) Gecko/20071127 Firefox/2.0.0.11'}
-		full_domain = 'http://' + domain
-		domain_request = Request(full_domain, headers=headers)
-		open_link = urlopen(domain_request)
-		findloginpage = FindLoginPage 	(full_domain, headers, sub_links, sub_link_count)
+			
+		domain_request = Request(full_domain, headers=UA)
+		open_link = urlopen(domain_request, timeout=TIMEOUT)
+		findloginpage = FindLoginPage 	(full_domain, sub_links, sub_link_count)
 		findloginpage.check()
 	except URLError:
-		print ("\nProvjerite unos, domena:", full_domain, "ne postoji.\n")
+		print ("\nCheck entered domain name:", full_domain, "doesn't exist.\n")
 		return
 	except HTTPError as greska:
-		print ("\nStranica nije dostupna. Greška:", greska.code)
+		print ("\nPage not found. Error nr.:", greska.code)
 		return
 	except IOError:
-		print ("Greška: nije bilo moguće otvoriti datoteku 'linkovi.txt', provjerite da li postoji datoteka.")
+		print ("Error: file can't be opened, check if file 'linkovi.txt' exist.")
 		return
-
-def credits	():
-	"""Printing credits"""
-	space(10); print ("\n\n")
-	space(10); print ("#####################################")
-	space(10); print ("#   Admin/Login Page Finder Script  #")
-	space(10); print ("#     Skriptu doradio Omer Ramić    #")
-	space(10); print ("#####################################")
-	space(10); print ("\n\n")
-		
-def space (j):
-	"""Making indentation for credit lines"""
-	i = 1
-	while i<=j:
-		print (end=' ')
-		i+=1
+	except KeyboardInterrupt:
+		print ("You hit control-c")
 
 def exit_gracefully(signum, frame):
 	"""Used for clean exit if user aborts with keyboard"""
 	signal(SIGINT, original_sigint)
 	try:
-		if input("\n\nStvarno želite izaći? (d/n)> ").lower().startswith('d'):
+		if input("\n\nReally exit? (y/n)> ").lower().startswith('y'):
 			exit(1)
 	except KeyboardInterrupt:
-		print("\n\nOk ok, Izlazim")
+		print("\n\nOk ok, exiting")
 		exit(1)
 		
 	signal(SIGINT, exit_gracefully)
@@ -99,8 +83,10 @@ def exit_gracefully(signum, frame):
 if __name__ == '__main__':
 	original_sigint = getsignal(SIGINT)
 	signal(SIGINT, exit_gracefully)
-	credits()
-	load_data()
-
-#Script made by Omer Ramić
-#Best regards
+	parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=AUTHOR)
+	parser.add_argument(dest = "URL", help = "Target URL (e.g. \"http://www.target.com\")")
+	args = parser.parse_args()
+	if args.URL:
+		load_data(args.URL if args.URL.startswith("http") else "http://%s" % args.URL)
+	else:
+		print (parser.print_help())
